141df66ff2e33b27b416f9cc9b4844bc1446079c
Matheus Dalbone <matheusdalbone7@gmail.com>
2024-05-20 22:44:25 -0300

    add Controller, Service, Repository

---
+++ src/main/java/br/org/serratec/ecommerce/controllers/EnderecoController.java
@@ -0,0 +1,5 @@
+package br.org.serratec.ecommerce.controllers;
+
+public class EnderecoController {
+
+}
---
+++ src/main/java/br/org/serratec/ecommerce/repositories/ItemPedidoRepository.java
@@ -0,0 +1,5 @@
+package br.org.serratec.ecommerce.repositories;
+
+public interface ItemPedidoRepository {
+
+}
