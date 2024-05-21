c2be30db863417f5e6c58ca06600d84c50cfdb0e
Miguel Caldas <miguelcaldas05@hotmail.com>
2024-05-21 16:07:34 -0300

    Implementação de Endereço

--- src/main/java/br/org/serratec/ecommerce/controllers/EnderecoController.java
+++ src/main/java/br/org/serratec/ecommerce/controllers/EnderecoController.java
@@ -1,5 +1,61 @@
 package br.org.serratec.ecommerce.controllers;
 
-public class EnderecoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.serratec.ecommerce.entities.Endereco;
+import br.org.serratec.ecommerce.services.EnderecoService;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+	
+	@Autowired
+	EnderecoService enderecoService;
+	
+	@GetMapping
+	public ResponseEntity<List<Endereco>> findAll(){
+		return new ResponseEntity<>(enderecoService.findAll(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Endereco> findById(@PathVariable Integer id){
+		Endereco endereco = enderecoService.findById(id);
+		if (endereco == null)
+			return new ResponseEntity<>(endereco, HttpStatus.NOT_FOUND);
+		else
+			return new ResponseEntity<>(endereco, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Endereco> save(@RequestBody Endereco endereco) {
+		return new ResponseEntity<>(enderecoService.save(endereco), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Endereco> update(@PathVariable Integer id, @RequestBody Endereco endereco) {
+		endereco = enderecoService.update(id,endereco);
+		return new ResponseEntity<>(endereco,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		Boolean deletado = enderecoService.delete(id);
+		if (deletado)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
 }
--- src/main/java/br/org/serratec/ecommerce/services/EnderecoService.java
+++ src/main/java/br/org/serratec/ecommerce/services/EnderecoService.java
@@ -1,5 +1,48 @@
 package br.org.serratec.ecommerce.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.ecommerce.entities.Endereco;
+import br.org.serratec.ecommerce.repositories.EnderecoRepository;
+
+@Service
 public class EnderecoService {
 
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	
+	public List<Endereco> findAll() {
+		return enderecoRepository.findAll();
+	}
+	
+	public Endereco findById(Integer id) {
+		return enderecoRepository.findById(id).orElse(null);
+	}
+	
+	public Endereco save(Endereco endereco) {
+		return enderecoRepository.save(endereco);
+	}
+	
+	public Endereco update(Integer id, Endereco endereco) {
+		Endereco entidade = enderecoRepository.getReferenceById(id);
+		return enderecoRepository.save(entidade);
+	}
+	
+	public Boolean delete(Integer id) {
+		if(enderecoRepository.existsById(id)) {
+			enderecoRepository.deleteById(id);
+			Endereco enderecoDeletado =  enderecoRepository.findById(id).orElse(null);
+			
+			if(enderecoDeletado == null) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 }
